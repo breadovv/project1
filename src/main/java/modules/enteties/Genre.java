3f5d545dfb4ea2c9c7f22e851843a3f614bea561
danialmarat <doni.danialmarat20066@gmail.com>
2025-02-07 10:57:09 +0500

    Added new entities; cart,game,genre

--- src/main/java/modules/enteties/Genre.java
+++ src/main/java/modules/enteties/Genre.java
@@ -1,4 +1,37 @@
 package modules.enteties;
 
-public class Genre {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "\"Genre\"")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Genre implements Basic<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genre_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Genre genre = (Genre) o;
+        return id != null && Objects.equals(id, genre.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
